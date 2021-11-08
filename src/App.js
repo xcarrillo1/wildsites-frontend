5c776862e0100eed4c586a10db1624f83d8fb874
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-11-07 22:20:46 -0500

    Rendered data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import Header from "./components/Header.js";
-import Main from "./components/Main.js";
-import Footer from "./components/Footer.js";
-import Sub from './components/Sub';
+// Import components //
+import Header from "./components/Header";
+import Main from "./components/Main";
 
+// API call function for animals
 function App() {
+  const [animalsState, setAnimalsState] = useState({ animals: [] });
+//
+useEffect(() => {
+  async function getAnimals() {
+    try {
+      const animals = await fetch('https://stormy-falls-16916.herokuapp.com/animals')
+      .then(response => response.json())
+      setAnimalsState({animals})
+    } catch (error) {
+      console.log(error)
+    }
+  }
+ 
+// Create
+async function handleAdd(formInputs) {
+    try {
+      const animals = await fetch('https://stormy-falls-16916.herokuapp.com/animals', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify(formInputs)
+      }).then(res => res.json())
+
+      setAnimalsState({ animals });
+      
+    } catch(error) {
+      console.log(error)
+    }
+  } 
+
+ getAnimals();
+}, []);
+  
   return (
     <div className="App">
       <Header />
-      <Sub />
-      <Main />
-      <Footer />
+      <Main animals={ animalsState.animals } />
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,78 +1,25 @@
-import { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Index from "../pages/Index";
-import Show from "../pages/Show";
-import Add from "../pages/Add";
-import About from "../pages/About";
+import react from "react";
+import { useState } from "react";
 
-function Main(props) {
-    const [animals, setAnimals] = useState(null);
+function Main ({animals}) {
+  
 
-    const URL = "https://stormy-falls-16916.herokuapp.com/animals";
-
-    const getAnimals = async () => {
-        const response = await fetch(URL);
-        const data = await response.json();
-        setAnimals(data);
-    };
-
-    const createAnimals = async (animals) => {
-        await fetch(URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "Application/json",
-            },
-            body: JSON.stringify(animals),
-        });
-        getAnimals();
-    };
-
-    const updateAnimals = async (animals, id) => {
-        await fetch(URL + id, { 
-            method: "PUT" , 
-            headers: {
-                "Content-Type": "Application/json", 
-            },
-            body: JSON.stringify(animals),
-        });
-        getAnimals();
-    }
-
-    const deleteAnimals = async id => {
-        await fetch(URL + id, { 
-            method: "DELETE", 
+  return (
+    <div>
+      <h1>main</h1>
+      {/*If animal is defined, display the animals  */}
+      {animals && 
+        animals.map((animal) => {
+          return (
+            <div>
+              <h1>{animal.name}</h1>
+              <img src={animal.image} />
+            </div>
+          )
         })
-        getAnimals();
-    }
-
-    useEffect(() => getAnimals(), []);
-
-    return (
-        <main>
-            <Routes>
-                <Route exact path="/">
-                    <Index animals={animals} createAnimals={createAnimals} />
-                </Route>
-                <Route path="/create">
-                    <Add animals={animals} createAnimals={createAnimals} />
-                </Route>
-                <Route
-                    path="/animals/:id"
-                    render={(rp) => (
-                        <Show
-                            animals={animals}
-                            updateAnimals={updateAnimals}
-                            deleteAnimals={deleteAnimals}
-                            {...rp}
-                        />
-                    )}
-                />
-                <Route path="/about">
-                     <About />   
-                </Route>
-            </Routes>
-        </main>
-    );
+      }
+    </div>
+  )
 }
 
-export default Main;
+export default Main
