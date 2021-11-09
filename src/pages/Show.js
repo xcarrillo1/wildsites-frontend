9371f18d2aa9e1dcd554f369e9dd175d77dcd924
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-11-09 18:04:34 -0500

    CSS styling

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,16 +1,19 @@
 import react from "react";
 import { useState } from "react";
 
-function Show(props) {
+function Show(props, handleDelete) {
   const id = props.match.params.id
   const animals = props.animals
   const animalX = animals.filter(p => p.id == id)[0]
-  console.log(animalX)
   return (
-  <div className="person">
-  <h1>{animalX.name}</h1>
-  <h2>{animalX.diet}</h2>
-  <img src={animalX.image} alt={animalX.name} />
+  <div className="animalShow">
+     <img src={animalX.image} alt={animalX.name} />
+    <h1>{animalX.name}</h1>
+     <p><b>What to do:</b> {animalX.action}</p>
+     <p><b>Type:</b> {animalX.diet}</p>
+     <p><b>Temperament:</b> {animalX.temp}</p>
+     <p><b>Location:</b> {animalX.location}</p>
+      {/* <button onClick={() => handleDelete(animals.id)} >X</button> */}
   </div>
 //     <div>
 //       {/* {animals.map((animal) => {
@@ -28,26 +31,3 @@ function Show(props) {
 
 export default Show;
 // In the new array, I want to get one animal's id and render that one animal's information
-
-
-// const animals = props.animals
-// const animal = animals.find(p => p._id === id)
-//   return (
-//     <div className="person">
-//     <h1>{animal.name}</h1>
-//     <h2>{animal.title}</h2>
-//     <img src={animal.image} alt={animal.name} />
-//   </div>
-//   )
-// } 
-
-{/* <div className="person">
-<h1>{animalX.name}</h1>
-<h2>{animal.title}</h2>
-<img src={animal.image} alt={animal.name} />
-</div> */}
-
-  // console.log(animals)
-  // const id = props.match.params.id
-  // const animal = props.animal
-  // const animalX = animal.find(p => p._id === id)
